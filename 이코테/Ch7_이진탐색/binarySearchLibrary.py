5ad1ad11eefda246ecacc3853f1e0285c1e174ff
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-01-14 00:40:49 +0900

    Binary Search

---
+++ 이코테/Ch7_이진탐색/binarySearchLibrary.py
@@ -0,0 +1,24 @@
+from bisect import bisect_left, bisect_right
+
+a = [1, 2, 4, 4, 8]
+x = 4
+
+print(bisect_left(a, x))
+print(bisect_right(a, x))
+
+# ( bisect_right 을 통해 구한 오른쪽 인덱스 ) - ( bisect_left 을 통해 구한 왼쪽 인덱스 )
+# -> 해당 값의 범위에 포함되어 있는 데이터 개수 구할 수 있음
+
+# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    return right_index - left_index
+
+# 배열 선언
+a = [1, 2, 3, 3, 3, 3, 4, 4, 8, 9]
+
+# 값이 4인 데이터 개수 출력
+print(count_by_range(a, 4, 4))   # left_value와 right_value 동일
+# 값이 [-1, 3] 범위에 있는 데이터 개수 출력
+print(count_by_range(a, -1, 3))
