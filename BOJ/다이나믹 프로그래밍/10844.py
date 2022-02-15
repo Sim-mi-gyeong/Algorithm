005129d97fd3613a18ffa640f7e323b21e329536
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-02-15 23:47:57 +0900

    DP

---
+++ BOJ/다이나믹 프로그래밍/10844.py
@@ -0,0 +1,14 @@
+# 쉬운 계단 수 
+
+n = int(input())
+dp = [0] * 10
+if n == 1:
+    for i in range(1, 10):
+        dp[i] = 1
+else:
+    for _ in range(n-1):
+        for i in range(1, 9):
+            dp[i] += 1
+        for j in range(9, 1-1, -1):
+            dp[j] += 1
+print(sum(dp) % 1000000000)
--- BOJ/다이나믹 프로그래밍/11057.py
+++ BOJ/다이나믹 프로그래밍/11057.py
@@ -1,1 +1,13 @@
 # 오르막 수
+
+n = int(input())
+dp = [0] * (10 + 1)
+for i in range(1, 10+1):
+    dp[i] = 1
+if n == 1:
+    print(sum(dp))
+else:
+    for _ in range(n):
+        for i in range(2, 10+1):
+            dp[i] = dp[i-1] + dp[i]   # dp[i] = 이전 회차의 dp 에서 i번째까지 원소의 합과 같음
+    print(dp[10] % 10007)
