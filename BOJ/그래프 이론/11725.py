0be4ff096049d409ff33045aa10e2faceeaff00a
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-02-21 23:39:52 +0900

    DFS

---
+++ BOJ/그래프 이론/11725.py
@@ -0,0 +1,40 @@
+# 트리의 부모 찾기
+
+import sys
+sys.setrecursionlimit(10**9)
+
+n = int(input())
+lst = [[] * (n + 1) for _ in range(n + 1)]
+for i in range(n-1):
+    a, b = map(int, input().split())
+    lst[a].append(b)
+    lst[b].append(a)
+
+parent = [0] * (n+1)
+
+def dfs(start, lst, parent):
+    for i in lst[start]:
+        if parent[i] == 0:
+            parent[i] = start
+            dfs(i, lst, parent)
+dfs(1, lst, parent)
+for i in range(2, len(parent)):
+    print(parent[i])
+
+# n = int(input())
+# lst = [[] * (n + 1) for _ in range(n + 1)]
+# for i in range(n-1):
+#     a, b = map(int, input().split())
+#     lst[a].append(b)
+#     lst[b].append(a)
+
+# parent = [0] * (n+1)
+# parent[1] = 1
+# for i in range(1, len(lst)):
+#     for j in lst[i]:
+#         if parent[j] != 0: continue
+#         elif parent[i] == j: continue
+#         else: parent[j] = i
+# for i in range(2, len(parent)):
+#     print(parent[i])
+
