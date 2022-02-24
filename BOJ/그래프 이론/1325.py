b256f06a17d6ee40f9b47f9a9dbcb9cdab5e77c6
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-02-25 00:51:55 +0900

    BFS

--- BOJ/그래프 이론/1325.py
+++ BOJ/그래프 이론/1325.py
@@ -1,33 +1,14 @@
 # 효율적인 해킹
 
 import sys
+from collections import deque
 input = sys.stdin.readline
-
-n, m = map(int, input().split())    # n : 컴퓨터 수, m : 관계 수
+n, m = map(int, input().split())
 rel = [[] for _ in range(n+1)]
-rel2 = [[] for _ in range(n+1)]
-visited = [0] * (n+1)
 for i in range(m):
     a, b = map(int, input().split())
-    rel[a].append(b)
-    rel[b].append(a)
-    rel2[a].append(b)
-print('rel : ', rel)
-print('rel2 : ', rel2)
-resutl = []
-
-from collections import deque
+    rel[b].append(a)   # 해킹의 주체는 b (b의 인덱스에 해킹 가능한 번호추가)
 queue = deque([])
-start = n
-# def bfs(start, rel, visited):
-#     queue.append(start)
-#     visited[start] += 1
-#     while queue:
-#         v = queue.popleft()
-#         for i in rel[v]:
-#             if visited[i] == 0:   # 방문하지 않았으면, 
-#                 visited[i] = visited[v] + 1
-#                 queue.append(i)
 
 def bfs(start, rel, visited):
     queue.append(start)
@@ -40,8 +21,17 @@ def bfs(start, rel, visited):
                 visited[i] = 1
                 queue.append(i)
                 cnt += 1
-    return cnt
+    return cnt   # return 값으로 특정한 값(해킹하는 컴퓨터의 수)을 반환할 때 -> visted[i] + 1 대신 cnt += 1
 
-print('bfs : ' , bfs(start, rel, visited))
+result = []
+maxCnt= 0
+for i in range(1, n+1):   # 시작 시점이 정해지지 않음
+    visited = [0] * (n+1)
+    ans = bfs(i, rel, visited)
+    if maxCnt < ans:
+        maxCnt = ans
+    result.append((i, ans))   # (index, cnt) 로 두 가지 정보 저장 가능 -> 따로, 최댓값일 때만 result로 업데이트 하지 않아도 됨
 
-print(visited)
+for i, ans in result:
+    if ans == maxCnt:
+        print(i, end = ' ')
