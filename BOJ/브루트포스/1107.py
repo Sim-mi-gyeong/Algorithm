f30c38851db60031c8350b6f3250481e9138e699
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-04-08 04:25:46 +0900

    Brute Force

--- BOJ/브루트포스/1107.py
+++ BOJ/브루트포스/1107.py
@@ -1,53 +1,36 @@
 # 리모컨
-#
-now = 100
+
 n = int(input())
-m = int(input())  # 고장 버튼 수
-if m >= 1:
-    lst = list(map(int, input().split()))  # 고장난 버튼 종류
+m = int(input())
+now = 100
+if m == 0:
+    ans = min(len(str(n)), abs(n - now))
+    print(ans)
+    exit(0)
 else:
-    lst = []
-
-a = []
-for i in range(1, 10):
-    if i not in lst:
-        a.append(i)
-
-if now == n:
+    lst = list(map(int, input().split()))
+if n == now:
     print(0)
     exit(0)
 
-cnt0, cnt1, cnt2 = 0, 0, 0
+cnt1 = abs(n - now)
+cnt2, tmpCnt = 500000, 500000
+for i in range(1000000 + 1):
+    breakCheck = 0
+    s = str(i)
+    for j in s:
+        if int(j) in lst:
+            breakCheck = 1
+            continue
+    if breakCheck != 1:  # 고장난 숫자가 없으면, 이 숫자와 target 간 + / - 로 이동할 때 횟수 저장
+        tmpNum = i
+        cnt2 = abs(n - tmpNum) + len(s)
+    tmpCnt = min(tmpCnt, cnt2)
 
-tmpMax = 0
-tmpMin = 0
-tmp = ""
-for i in a:
-    tmp += str(i)
-    tmpMin = min(tmpMin, int(tmp))
+ans = min(cnt1, tmpCnt)
+print(ans)
 
-while True:
-    for i in a:
-        tmp += str(i)
-        tmpMin = min(tmpMin, int(tmp))
-
-    if abs(tmpMin - n) < abs(tmpMax - n):
-        cnt1 += tmpMin - n
-    else:
-        cnt2 += tmpMax - n
-
-
-cnt0 = n - now
-
-
-# tmp = ""
-# for i in str(n):
-#     if int(i) not in lst:
-#         tmp += i
-#         cnt += 1
-#     print("tmp : ", tmp, "일 때 cnt : ", cnt)
-
-# if cnt != len(str(n)):
-#     pass
-
-# print(cnt)
+# 가능한 버튼으로 target 채널과 가장 가까운 채널 찾는 방법 ? => Brute Force
+# Brute Froce = Case 분류
+# 1) 현재 채널에서 + / - 버튼으로만 이동
+# 2) 모든 채널 순회 + 해당 채널에서 희망 채널까지 + / - 버튼으로 이동
