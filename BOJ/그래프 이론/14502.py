0ad6236e61c321ef6e30f4a80347b63ec1f233d2
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-04-23 01:32:29 +0900

    BFS

--- BOJ/그래프 이론/14502.py
+++ BOJ/그래프 이론/14502.py
@@ -1,88 +1,60 @@
 # 연구소
 
 from collections import deque
+import sys
+import copy
 
-n, m = map(int, input().split())  # 세로, 가로
-lst = [list(map(int, input().split())) for _ in range(n)]
-# 0: 빈칸, 1: 벽, 2: 바이러스
-# 바이러스: 상하좌우 인접한 빈 공간으로 퍼져 나감
-# 새로 세울 수 있는 벽의 수 = 3
-# 안전영역 최대 크기
-# 꽃길 문제랑 유사?
-
+input = sys.stdin.readline
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 visited = [[0] * m for _ in range(n)]
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-virusCnt = 0
 
 
-q = deque()
-
-# 처음 바이러스가 2인 곳을 큐에 넣고 -> BFS
-for i in range(n):
-    for j in range(m):
-        if lst[i][j] == 2:
-            visited[i][j] = 1
-            q.append((i, j))
-            virusCnt += 1
-            print("바이러스 (i, j) : ", (i, j))
-
-
-def virus(lst):
-    global virusCnt
-    while q:
-        x, y = q.popleft()
-        for i in range(len(dx)):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < m:
-                if lst[nx][ny] == 1:
-                    visited[nx][ny] = 1
-                    continue
-                elif lst[nx][ny] == 0 and visited[nx][ny] == 1:
-                    continue
-                elif lst[nx][ny] == 0 and visited[nx][ny] != 1:
-                    # lst[nx][ny] = 2
-                    visited[nx][ny] = 1
-                    q.append((nx, ny))
-                    virusCnt += 1
-                    print("바이러스 (nx, ny) : ", (nx, ny))
-
-    return virusCnt
-
-
-wallCnt = 0
-maxSize = 0
-for i in range(n):
-    for j in range(m):
-        if lst[i][j] == 1:
-            visited[i][j] = 1
-            wallCnt += 1
-
-
-def bfs(cnt, wallCnt):
-    global maxSize
+def wall(cnt):
+    global wallCnt
     if cnt == 3:
-        # 바이러스 전파
-        virusCnt = virus(lst)
-        size = n * m - virusCnt - wallCnt
-        print("virusCnt : ", virusCnt, " wallCnt : ", wallCnt)
-        maxSize = max(maxSize, size)
-
+        bfs()
         return
 
     for i in range(n):
         for j in range(m):
-            if visited[i][j] == 0 and lst[i][j] == 0:
-                visited[i][j] = 1
-                wallCnt += 1
-
-                bfs(cnt + 1, wallCnt)
-
-                # 벽 세우기 전으로 초기화(백트래킹)
-                visited[i][j] = 0
-                wallCnt -= 1
+            if graph[i][j] == 0:
+                graph[i][j] = 1
+                wall(cnt + 1)
+                graph[i][j] = 0
 
 
-bfs(0, wallCnt)
+maxSize = 0
+
+
+def bfs():
+    global maxSize
+    copyGraph = copy.deepcopy(graph)
+
+    queue = deque()
+
+    for i in range(n):
+        for j in range(m):
+            if copyGraph[i][j] == 2:
+                # visited[i][j] = 1  # 방문처리 -> 이걸 빼야 하는게, 위에서 wall 함수에서는 백트래킹 시 visited 도 이전 값으로 돌렸는데, bfs 에서는 이전으로 visited 상태를 돌리는 부분이 없었음
+                queue.append((i, j))
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(len(dx)):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if copyGraph[nx][ny] == 0:
+                    copyGraph[nx][ny] = 2
+                    queue.append((nx, ny))
+
+    safeSize = 0
+    for i in range(n):
+        safeSize += copyGraph[i].count(0)
+    maxSize = max(maxSize, safeSize)
+
+
+wall(0)
 print(maxSize)
