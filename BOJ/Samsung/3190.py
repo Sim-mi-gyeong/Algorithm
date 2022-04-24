777f15146e458ef51269b67992c1e2b26729c8ab
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-04-25 00:17:18 +0900

    BFS

--- BOJ/Samsung/3190.py
+++ BOJ/Samsung/3190.py
@@ -3,9 +3,6 @@ from collections import deque
 
 n = int(input())
 graph = [[0] * n for _ in range(n)]
-visited = [[0] * n for _ in range(n)]
-
-startX, startY = 0, 0
 k = int(input())
 for _ in range(k):
     i, j = map(int, input().split())
@@ -21,46 +18,44 @@ for _ in range(l):
 
 
 time = 0
+startX, startY = 0, 0
 d = 0  # 현재 방향 동쪽
 r = [0, 1, 2, 3]  # 동 - 남 - 서 - 북
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 length = 1
-# 사과가 있어서 몸 길이를 늘렸다가 -> 다음 칸에 사과가 없으면?
-tailX, tailY = startX, startY
 
 
-def bfs(startX, startY, tailX, tailY):
+def bfs(x, y):
     global time, length
-
-    queue = deque()
-    queue.append((startX, startY, tailX, tailY))
+    graph[x][y] = 1
+    moveQ = deque()
+    moveQ.append((x, y))  # 뱀의 이동 위치
     d = 0
+
     while True:
         time += 1
-        x, y, tailX, tailY = queue.popleft()
         nx = x + dx[d]
         ny = y + dy[d]
 
         if nx < 0 or ny < 0 or nx >= n or ny >= n or graph[nx][ny] == 1:
-            breakX, breakY = nx, ny
-            return time, breakX, breakY, graph[breakX][breakY]
+            return time
         else:
-            if graph[nx][ny] == 2:  # 사과가 위치한 경우, 방문 처리 & 몸 길이 늘리기
+            if graph[nx][ny] == 2:
                 graph[nx][ny] = 1
+                moveQ.append((nx, ny))
                 length += 1
-            else:  # 사과가 없는 경우, 길이가 1로 원상복구 되나? / 방문처리 -> 이전에 꼬리가 있는 경우는? 0으로 어떻게 ?
-                # 머리 제외 전부 0으로 돌아가는가?
+            elif graph[nx][ny] == 0:
                 graph[nx][ny] = 1
-                graph[tailX][tailY] = 0  # 이동 전 꼬리 부분을 빈 칸으로
-                tailX = tailX + dx[d]  # 꼬리 앞으로 당기기
-                tailY = tailY + dy[d]
+                moveQ.append((nx, ny))
+
+                tailX, tailY = moveQ.popleft()  # 가장 끝부분 꼬리 비우기
+                graph[tailX][tailY] = 0
 
             if length > n:
-                breakX, breakY = nx, ny
-                return time, breakX, breakY
+                return time
 
-            queue.append((nx, ny, tailX, tailY))
+            x, y = nx, ny
 
         if time in dirTime:
             if dir[time] == "D":
@@ -69,4 +64,4 @@ def bfs(startX, startY, tailX, tailY):
                 d = (d + 3) % 4
 
 
-print(bfs(startX, startY, tailX, tailY))
+print(bfs(startX, startY))
