0e8a16455284c1433d0bd25c5d81f1c8c230659a
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-06-15 22:39:27 +0900

    BFS

---
+++ BOJ/그래프 이론/5547.py
@@ -0,0 +1,13 @@
+# 일루미네이션
+
+w, h = map(int, input().split())  # 가로, 세로 1부터
+graph = [list(map(int, input().split())) for _ in range(h)]  # 건물 O : 1, X : 0
+
+ans = 0
+
+
+def bfs():
+    pass
+
+
+print(ans)
