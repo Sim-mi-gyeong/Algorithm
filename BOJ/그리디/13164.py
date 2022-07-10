eee79f5de465960658039414698c5e57fcb90430
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-07-10 23:07:58 +0900

    Greedy

--- BOJ/그리디/13164.py
+++ BOJ/그리디/13164.py
@@ -1,1 +1,16 @@
 # 행복 유치원
+n, k = map(int, input().split())
+lst = list(map(int, input().split()))
+lst.sort()
+
+diff = []
+for i in range(n - 1):
+    diff.append(lst[i + 1] - lst[i])
+
+diff.sort(reverse=True)
+
+for i in range(k - 1):
+    if i > len(diff) - 1:
+        continue
+    diff[i] = 0
+print(sum(diff))
