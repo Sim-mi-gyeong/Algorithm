9780bc4e4d45879635008670f11398bf2ab82ed5
Sim_migyeong <smegyeong@gmail.com>
2022-09-23 01:05:26 +0900

    BFS

--- BOJ/그래프 이론/18405.py
+++ BOJ/그래프 이론/18405.py
@@ -1,4 +1,5 @@
 # 경쟁적 전염
+
 import sys
 from collections import deque
 
@@ -6,54 +7,29 @@ input = sys.stdin.readline
 n, k = map(int, input().split())
 
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-virus = dict()
+virus = []  # 처음 각 바이러스는 여러 개 존재 가능 -> 딕셔너리 사용 시 한 종류의 바이러스에 대해 한 개의 위치만 저장
 graph = []
 for i in range(n):
     tmp = list(map(int, input().split()))
     graph.append(tmp)
-    for j in range(len(tmp)):
-        if tmp[j] != 0 and tmp[j] not in virus:
-            virus[tmp[j]] = (i, j)
+    for j in range(n):
+        if graph[i][j] != 0:
+            virus.append((graph[i][j], i, j, 0))
 
 s, targetX, targetY = map(int, input().split())
-virus = dict(sorted(virus.items()))
-visited = [[0] * n for _ in range(n)]
+virus = sorted(virus, key=lambda x: x[0])
+q = deque(virus)
 
+while q:
+    num, x, y, time = q.popleft()
+    if time == s:
+        break
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n:
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = num
+                q.append((num, nx, ny, time + 1))
 
-def bfs(graph, q, visited):
-    while q:
-        x, y, num, time = q.popleft()
-        if time == s + 1:
-            return graph[targetX - 1][targetY - 1]
-
-        for i in range(len(dx)):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-                if graph[nx][ny] == 0:
-                    visited[nx][ny] = 1
-                    graph[nx][ny] = num
-                    q.append((nx, ny, graph[nx][ny], time + 1))
-
-    return graph[targetX - 1][targetY - 1]
-
-
-q = deque()
-for key, val in virus.items():
-    q.append((val[0], val[1], key, 1))
-    visited[val[0]][val[1]] = 1
-
-print(bfs(graph, q, visited))
-
-
-"""
-4 2
-1 0 0 0
-0 0 0 0
-0 0 0 0
-0 0 0 2
-3 3 2
-
-answer : 1
-"""
-
+print(graph[targetX - 1][targetY - 1])
