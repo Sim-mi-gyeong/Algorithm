8c43f22a87b66d6fe06da3be904437361c9688f6
Sim_migyeong <smegyeong@gmail.com>
2022-10-20 02:50:53 +0900

    Implementation

--- BOJ/구현/16927.py
+++ BOJ/구현/16927.py
@@ -1,44 +1,47 @@
-# 배열 돌리기 2
+# 배열 돌리기2
 
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 n, m, r = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-circleCnt = m // 2
+circleCnt = min(n, m) // 2
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
 
 def rotate():
-    startX, endX = 0, n - 1
-    startY, endY = 0, m - 1
-
-    new_graph = [[] for _ in range(n)]
-    for i in range(n):
-        new_graph[i] = graph[i][:]
+    q = deque()
 
     for circle in range(circleCnt):
-        x, y = startX, startY
+        x, y = circle, circle
         for dir in range(len(dx)):
             while True:
                 nx = x + dx[dir]
                 ny = y + dy[dir]
-                if startX <= nx <= endX and startY <= ny <= endY:
-                    graph[nx][ny] = new_graph[x][y]
+                if circle <= nx < n - circle and circle <= ny < m - circle:
+                    q.append(graph[x][y])
                     x, y = nx, ny
                 else:
                     break
 
-        startX += 1
-        endX -= 1
-        startY += 1
-        endY -= 1
+        num = r % ((n - circle * 2) * 2 + (m - circle * 2) * 2 - 4)  # 각 변에 있는 칸의 개수 - 중복되는 칸의 개수
+        for _ in range(num):
+            q.appendleft(q.pop())
+
+        for dir in range(len(dx)):
+            while True:
+                nx = x + dx[dir]
+                ny = y + dy[dir]
+                if circle <= nx < n - circle and circle <= ny < m - circle:
+                    graph[x][y] = q.popleft()
+                    x, y = nx, ny
+                else:
+                    break
 
 
-for _ in range(r):
-    rotate()
-
+rotate()
 for i in range(n):
     print(*graph[i])
