6776b850830d7dc3d2521f67c61edd4674c22f8e
Sim_migyeong <smegyeong@gmail.com>
2022-11-03 23:07:28 +0900

    Backtracking

---
+++ BOJ/백트래킹/1759.py
@@ -0,0 +1,19 @@
+# 암호 만들기
+# 최소 한 개의 모음 + 최소 두 개의 자음
+# 오름차순 정렬
+
+l, c = map(int, input().split())
+lst = list(input().split())
+lst = sorted(lst)
+tmp = ""
+m = ("a", "e", "i", "o", "u")
+
+
+def dfs(tmp, depth):
+    if depth == l:
+
+        return
+
+    while len(tmp) < l:
+        for i in range(len(lst)):
+            tmp += lst[i]
