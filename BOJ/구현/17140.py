69f6b2e68d666fbcc8ed73d887070b809e881797
Sim_migyeong <smegyeong@gmail.com>
2022-11-08 23:19:33 +0900

    Implementation

--- BOJ/구현/17140.py
+++ BOJ/구현/17140.py
@@ -1,7 +1,5 @@
 # 이차원 배열과 연산
 import sys
-import copy
-from collections import Counter
 
 input = sys.stdin.readline
 r, c, k = map(int, input().split())
@@ -9,72 +7,99 @@ r, c, k = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(3)]
 
 
-def cal_r(copy_arr):
+def cal_r(arr):
     new_arr = []
-    for row in copy_arr:
-        max_row_num = 0
-
-        row_counter_dict = Counter(row)
-        row_counter_dict = dict(sorted(row_counter_dict.items(), key=lambda x: (x[1], x[0])))
-
+    max_row_num = 0
+    for row in arr:
         tmp_arr = []
-        for key, val in row_counter_dict.items():
-            tmp_arr.append(key)
-            tmp_arr.append(val)
+        set_row = set(row)
+        for tmp_set_row in set_row:
+            if tmp_set_row == 0:
+                continue
+            tmp_set_row_cnt = row.count(tmp_set_row)
+            tmp_arr.append((tmp_set_row, tmp_set_row_cnt))
 
-        if max_row_num < len(tmp_arr):
-            max_row_num = len(tmp_arr)
+        tmp_arr = sorted(tmp_arr, key=lambda x: (x[1], x[0]))
+        new_tmp_arr = []
+        for num, cnt in tmp_arr:
+            new_tmp_arr.append(num)
+            new_tmp_arr.append(cnt)
 
-        new_arr.append(tmp_arr)
+        max_row_num = max(max_row_num, len(new_tmp_arr))
+
+        new_arr.append(new_tmp_arr)
+
+    for row in new_arr:
+        if len(row) > 100:
+            row = row[:100]
+            continue
+        if len(row) < max_row_num:
+            row += [0] * (max_row_num - len(row))
 
     return new_arr
 
 
-def cal_c(copy_arr):
+def cal_c(arr):
+    arr = list(zip(*arr))
     new_arr = []
-    for row_idx in range(len(copy_arr)):
-        col = copy_arr[row_idx][:]
-
-        col_counter_dict = Counter(col)
-        col_counter_dict = dict(sorted(col_counter_dict.items(), key=lambda x: (x[1], x[0])))
-
+    max_row_num = 0
+    for row in arr:
         tmp_arr = []
-        for key, val in col_counter_dict.items():
-            tmp_arr.append(key)
-            tmp_arr.append(val)
+        set_row = set(row)
+        for tmp_set_row in set_row:
+            if tmp_set_row == 0:
+                continue
+            tmp_set_row_cnt = row.count(tmp_set_row)
+            tmp_arr.append((tmp_set_row, tmp_set_row_cnt))
 
-        new_arr.append(tmp_arr)
+        tmp_arr = sorted(tmp_arr, key=lambda x: (x[1], x[0]))
+        new_tmp_arr = []
+        for num, cnt in tmp_arr:
+            new_tmp_arr.append(num)
+            new_tmp_arr.append(cnt)
+
+        max_row_num = max(max_row_num, len(new_tmp_arr))
+
+        new_arr.append(new_tmp_arr)
+
+    for row in new_arr:
+        if len(row) > 100:
+            row = row[:100]
+            continue
+        if len(row) < max_row_num:
+            row += [0] * (max_row_num - len(row))
+
+    new_arr = list(zip(*new_arr))
 
     return new_arr
 
 
-def check():
+def check(arr):
+    if len(arr) < r or len(arr[0]) < c:
+        return False
     if arr[r - 1][c - 1] == k:
         return True
     return False
 
 
-time = 0
-
-
 def cal():
     global arr
-
     row_num = len(arr)
     col_num = len(arr[0])
-    copy_arr = copy.deepcopy(arr)
 
     if row_num >= col_num:
-        arr = cal_r(copy_arr)
+        arr = cal_r(arr)
     else:
-        arr = cal_c(copy_arr)
+        arr = cal_c(arr)
 
 
+time = 0
 while True:
     if time > 100:
         print(-1)
         break
-    if check():
+
+    if check(arr):
         print(time)
         break
 
