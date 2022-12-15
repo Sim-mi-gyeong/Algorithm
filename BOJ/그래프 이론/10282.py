35958b2beaeac762c61b7ddec726ecc2b1df4ee6
Sim_migyeong <smegyeong@gmail.com>
2022-12-15 23:32:14 +0900

    Create 10282.py

---
+++ BOJ/그래프 이론/10282.py
@@ -0,0 +1,21 @@
+# 해킹
+
+import sys
+
+input = sys.stdin.readline
+
+
+def solve():
+    cnt, time = 0, 0
+
+    return cnt, time
+
+
+t = int(input())
+for _ in range(t):
+    n, d, c = map(int, input().split())
+    for _ in range(d):
+        a, b, s = map(int, input().split())
+
+        cnt, time = solve()
+        print(cnt, time)
