61a44ff1681a7c0ade57a06f891123adc4fec3e9
Sim_migyeong <smegyeong@gmail.com>
2022-12-24 21:31:08 +0900

    BFS

--- BOJ/그래프 이론/16594.py
+++ BOJ/그래프 이론/16594.py
@@ -5,15 +5,11 @@ import sys
 
 input = sys.stdin.readline
 
-graph = []
-wall = deque()
+graph = deque()
 n = 8
 for i in range(n):
     tmp = list(input())
     graph.append(tmp)
-    for j in range(n):
-        if tmp[j] == "#":
-            wall.append((i, j))
 
 dx = [0, -1, -1, 0, 1, 1, 1, 0, -1]
 dy = [0, 0, 1, 1, 1, 0, -1, -1, -1]
@@ -24,33 +20,34 @@ endX, endY = 0, 7
 
 def bfs(startX, startY):
     q = deque()
-    visited = [[0] * 8 for _ in range(8)]
-    q.append((startX, startY, 0))
-    visited[startX][startY] = 1
+    q.append((startX, startY))
 
+    # time = 0
     while q:
-        x, y, time = q.popleft()
-        if (x, y) == (endX, endY):
-            return 1
-        if graph[x][y] == "#":
-            return 0
+        visited = [[0] * 8 for _ in range(8)]
+        qNum = len(q)
+        for _ in range(qNum):
+            x, y = q.popleft()
 
-        for i in range(len(dx)):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and graph[nx][ny] == ".":
-                visited[nx][ny] = 1
-                q.append((nx, ny, time + 1))
+            if (x, y) == (endX, endY):
+                return 1
 
-        wallNum = len(wall)
-        for _ in range(wallNum):
-            wallX, wallY = wall.popleft()
-            if 0 <= wallX + 1 < n:
-                graph[wallX + 1][wallY] = "#"
-                graph[wallX][wallY] = "."
-                wall.append((wallX + 1, wallY))
-            else:
+            if graph[x][y] == "#":
                 continue
+
+            for i in range(len(dx)):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and graph[nx][ny] == ".":
+                    visited[nx][ny] = 1
+                    q.append((nx, ny))
+
+        graph.pop()
+        graph.appendleft([".", ".", ".", ".", ".", ".", ".", "."])
+
+        # time += 1
+        # if time == 9:
+        #     return 1
 
     return 0
 
