83c2a056d9c67b651715bb73d61304a467bfebe5
Sim_migyeong <smegyeong@gmail.com>
2023-01-03 23:17:27 +0900

    Union Find

--- BOJ/자료구조/1043.py
+++ BOJ/자료구조/1043.py
@@ -1,9 +1,11 @@
 # 거짓말
 
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
 
 
 def find_parent(x, parent):
@@ -15,29 +17,49 @@ def find_parent(x, parent):
 def union(a, b):
     parentA = find_parent(a, parent)
     parentB = find_parent(b, parent)
-
-    if parentA != parentB:
+    if parentA in knowpeople and parentB in knowpeople:
+        return
+    elif parentA in knowpeople:
         parent[parentB] = parentA
+    elif parentB in knowpeople:
+        parent[parentA] = parentB
+    else:
+        if parentA < parentB:
+            parent[parentB] = parentA
+        else:
+            parent[parentA] = parentB
 
 
-parent = [0] * (n + 1)
 knowPeopleNum, *knowpeople = map(int, input().split())
-for p in knowpeople:
-    parent[p] = 1
 
 
 def solve(partyPeopleNum, partyPeople):
-    check = True
-    for p in partyPeople:
-        if parent[p] != 0:
-            check = False
+    q = deque(partyPeople)
+    start = q.popleft()
+    while q:
+        p = q.popleft()
+        union(start, p)
+        start = p
 
-    return check
+
+party = []
+for _ in range(m):
+    partyPeopleNum, *partyPeople = map(int, input().split())
+    party.append(partyPeople)
+    solve(partyPeopleNum, partyPeople)
+
+
+def checkEnable():
+    global ans
+    for tmpPatry in party:
+        check = True
+        for person in tmpPatry:
+            if find_parent(person, parent) in knowpeople:
+                check = False
+        if check:
+            ans += 1
 
 
 ans = 0
-for _ in range(m):
-    partyPeopleNum, *partyPeople = map(int, input().split())
-    if solve(partyPeopleNum, partyPeople):
-        ans += 1
+checkEnable()
 print(ans)
