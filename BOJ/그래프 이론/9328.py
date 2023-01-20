7add9051a84b8f9b3d4756bde35159347e28a52f
Sim_migyeong <smegyeong@gmail.com>
2023-01-20 23:49:46 +0900

    BFS

---
+++ BOJ/그래프 이론/9328.py
@@ -0,0 +1,27 @@
+# 열쇠
+
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+
+def solve():
+
+    ans = 0
+
+    return ans
+
+
+t = int(input().rstrip())
+for _ in range(t):
+    h, w = map(int, input().rstrip().split())
+    graph = []
+    for i in range(h):
+        tmp = list(input().rstrip())
+        graph.append(tmp)
+        for j in range(w):
+            pass
+    key = list(input().rstrip())
+
+    ans = solve()
