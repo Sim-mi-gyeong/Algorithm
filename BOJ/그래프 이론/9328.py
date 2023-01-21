f34b6604e056df11b7983afbcd25ae70aae628ae
Sim_migyeong <smegyeong@gmail.com>
2023-01-21 23:54:38 +0900

    BFS

--- BOJ/그래프 이론/9328.py
+++ BOJ/그래프 이론/9328.py
@@ -17,11 +17,19 @@ t = int(input().rstrip())
 for _ in range(t):
     h, w = map(int, input().rstrip().split())
     graph = []
+    doc = dict()
     for i in range(h):
         tmp = list(input().rstrip())
         graph.append(tmp)
+        smallKey = dict()
+        largeKey = dict()
         for j in range(w):
-            pass
+            if tmp[j] == "$":  # 문서의 위치 저장
+                doc[(i, j)] = 0
+            elif tmp[j].isalpha() and tmp[j].islower():
+                smallKey[(i, j)] = 0
+            elif tmp[j] == " ":
+                largeKey[(i, j)] = 0
     key = list(input().rstrip())
 
     ans = solve()
