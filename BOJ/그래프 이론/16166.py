62fcc295d6bbf49e3a49f94f7bcc269989d53493
Sim_migyeong <smegyeong@gmail.com>
2023-03-06 23:57:24 +0900

    Create 16166.py

---
+++ BOJ/그래프 이론/16166.py
@@ -0,0 +1,11 @@
+# 서울의 지하철
+
+
+n = int(input())
+line = [[] for _ in range(n + 1)]
+for i in range(n):
+    lst = list(map(int, input().split()))
+    for j in range(len(lst)):
+        pass
+
+start = 0
