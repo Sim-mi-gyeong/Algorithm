02706503b99b3e6aff930d0f239c3af94d1b94d0
Sim_migyeong <smegyeong@gmail.com>
2023-03-25 20:55:06 +0900

    Queue

---
+++ BOJ/자료구조/12873.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+n = int(input())
+q = deque([i for i in range(1, n + 1)])
+currStep = 1
+currIdx = 0
+remain_n = n
+while len(q) > 1:
+    tmpT = currStep ** 3 % len(q)
+
+    if tmpT == 0:
+        tmpT = len(q)
+
+    for _ in range(tmpT - 1):
+        head = q.popleft()
+        q.append(head)
+
+    q.popleft()
+    currStep += 1
+
+print(q[0])
