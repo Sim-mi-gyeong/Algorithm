694a88fb2a0137157f89b74d5444a56ea3286ecd
Sim_migyeong <smegyeong@gmail.com>
2023-04-30 18:04:10 +0900

    Queue

--- Programmers/스택_큐/프로세스.py
+++ Programmers/스택_큐/프로세스.py
@@ -21,3 +21,74 @@ def solution(priorities, location):
             val.pop(0)
 
     return answer
+
+
+from collections import deque
+
+
+def solution2(priorities, location):
+    answer = 0
+
+    q = deque()  # q = [(i,p) for i,p in enumerate(priorities)]
+    for i in range(len(priorities)):
+        q.append((i, priorities[i]))
+
+    dic = dict()
+
+    turn = 1
+    while q:
+        idx, priority = q.popleft()
+        if q:
+            high = max(q, key=lambda x: x[1])[1]
+        # 마지막 남은 프로세스라면, 큐에서 꺼낸 후 빈 상태가 되므로 -> high 는 이전에 꺼낸 프로세스
+        if len(q) > 0 and priority < high:
+            q.append((idx, priority))
+            continue
+        if len(priorities) == 0:
+            dic[idx] = turn
+
+        dic[idx] = turn
+
+        if location == idx:
+            answer = turn
+            break
+
+        turn += 1
+
+    return answer
+
+
+def solution3(priorities, location):
+    answer = 0
+
+    process = dict()
+    q = deque()
+    priorities = deque(priorities)
+    for i in range(len(priorities)):
+        process[i] = priorities[i]
+        q.append((i, priorities[i]))
+
+    dic = dict()
+
+    turn = 1
+    while q:
+        idx, priority = q.popleft()
+        curr = priorities.popleft()
+        if priorities:
+            high = max(priorities)
+        if len(priorities) > 0 and curr < high:
+            q.append((idx, priority))
+            priorities.append(curr)
+            continue
+        if len(priorities) == 0:
+            dic[idx] = turn
+
+        dic[idx] = turn
+
+        if location == idx:
+            answer = turn
+            break
+
+        turn += 1
+
+    return answer
