5f071eae9e22730ec4c584617034f9ae2a39c677
Sim_migyeong <smegyeong@gmail.com>
2023-06-08 21:00:00 +0000

    Update 택배 배달과 수거하기.py

--- Programmers/KAKAO 2023/택배 배달과 수거하기.py
+++ Programmers/KAKAO 2023/택배 배달과 수거하기.py
@@ -1,3 +1,15 @@
 def solution(cap, n, deliveries, pickups):
-    answer = -1
+    answer = 0
+
+    tmpDeiveries, tmpPickups = 0, 0
+    for i in range(n):
+        tmpDeiveries += deliveries[n - i - 1]
+        tmpPickups += pickups[n - i - 1]
+
+        while tmpDeiveries > 0 or tmpPickups > 0:
+            tmpDeiveries -= cap
+            tmpPickups -= cap
+
+            answer += (n - i) * 2
+
     return answer
