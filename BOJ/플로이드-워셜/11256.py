1ed88fbf1cc8b465b0953a350c537c7429e6283a
Sim_migyeong <smegyeong@gmail.com>
2023-08-12 19:10:57 +0900

    Update 11256.py

--- BOJ/플로이드-워셜/11256.py
+++ BOJ/플로이드-워셜/11256.py
@@ -1,23 +1,32 @@
 # 끝나지 않는 파티
 
-
 import sys
 
 input = sys.stdin.readline
-
 n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+
+graph = [[0] * n for _ in range(n)]
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    for j in range(n):
+        graph[i][j] = tmp[j]
 
 for k in range(n):
     for i in range(n):
         for j in range(n):
-            if graph[i][j] > graph[i][k] + graph[k][j]:
-                graph[i][j] = graph[i][k] + graph[k][j]
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+
+def check(src, dst, time):
+    if graph[src - 1][dst - 1] <= time:
+        return True
+    return False
+
 
 for _ in range(m):
-    a, b, time = map(int, input().split())
-    if graph[a - 1][b - 1] <= time:
+    a, b, c = map(int, input().split())
+
+    if check(a, b, c):
         print("Enjoy other party")
     else:
         print("Stay here")
-
